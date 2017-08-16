021bcc0603a8073566faa2fbe3e613eee594f06c
notequals <hshsiao63@gmail.com>
2017-08-16 16:53:22 -0700

    update level editor

---
+++ Assets/StageEditor/EditorGameObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorGameObject : MonoBehaviour {
+
+    public Sprite sprite;
+    public string objectName;
+
+}
